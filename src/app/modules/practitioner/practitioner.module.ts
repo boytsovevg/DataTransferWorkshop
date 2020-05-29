e3b66d428cb82a787b6fa8a3deca38e2045372c5
Evgeniy Boytsov <evgeniy.boytsov@gmail.com>
2020-05-29 16:23:37 +0300

    add data provider

--- src/app/modules/practitioner/practitioner.module.ts
+++ src/app/modules/practitioner/practitioner.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { PractitionersPreviewContainerComponent } from './containers/practitioners-preview-container/practitioners-preview-container.component';
 import { PersonModule } from '../../components/person/person.module';
 import { PractitionersStoreModule } from './store/practitioners-store.module';
+import { PractitionersDataService } from './data/practitioners-data.service';
 
 @NgModule({
     imports: [CommonModule, PersonModule, PractitionersStoreModule],
     declarations: [PractitionersPreviewContainerComponent],
+    providers: [PractitionersDataService],
     exports: [PractitionersPreviewContainerComponent],
 })
 export class PractitionerModule {}
